b660115792f1492b9af28b612fa2311dec86c0e7
Michael <Michael.Breeden88@gmail.com>
2017-06-06 22:36:18 -0500

    Add SQL Scripts
    Add SQL Scripts

---
+++ ClotheOurKids.Model/SQL_Scripts/002_Create_dbo_AspNetUsers.sql
@@ -0,0 +1,36 @@
+﻿USE [ClotheOurKids]
+GO
+
+/****** Object: Table [dbo].[AspNetUsers] Script Date: 5/24/2017 10:07:14 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AspNetUsers] (
+    [Id]                   NVARCHAR (128) NOT NULL,
+    [Email]                NVARCHAR (256) NULL,
+    [EmailConfirmed]       BIT            NOT NULL,
+    [PasswordHash]         NVARCHAR (MAX) NULL,
+    [SecurityStamp]        NVARCHAR (MAX) NULL,
+    [PhoneNumber]          NVARCHAR (MAX) NULL,
+    [PhoneNumberConfirmed] BIT            NOT NULL,
+    [TwoFactorEnabled]     BIT            NOT NULL,
+    [LockoutEndDateUtc]    DATETIME       NULL,
+    [LockoutEnabled]       BIT            NOT NULL,
+    [AccessFailedCount]    INT            NOT NULL,
+    [UserName]             NVARCHAR (256) NOT NULL
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [UserNameIndex]
+    ON [dbo].[AspNetUsers]([UserName] ASC);
+
+
+GO
+ALTER TABLE [dbo].[AspNetUsers]
+    ADD CONSTRAINT [PK_dbo.AspNetUsers] PRIMARY KEY CLUSTERED ([Id] ASC);
+
+
---
+++ ClotheOurKids.Model/SQL_Scripts/003_Create_dbo_AspNetRoles.sql
@@ -0,0 +1,26 @@
+﻿USE [ClotheOurKids]
+GO
+
+/****** Object: Table [dbo].[AspNetRoles] Script Date: 5/24/2017 9:59:17 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AspNetRoles] (
+    [Id]   NVARCHAR (128) NOT NULL,
+    [Name] NVARCHAR (256) NOT NULL
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [RoleNameIndex]
+    ON [dbo].[AspNetRoles]([Name] ASC);
+
+
+GO
+ALTER TABLE [dbo].[AspNetRoles]
+    ADD CONSTRAINT [PK_dbo.AspNetRoles] PRIMARY KEY CLUSTERED ([Id] ASC);
+
+
